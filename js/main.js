43360941349ebe10821c3cde3e0e25e234faa08b
WAPEETY <davide.antonio.pz@gmail.com>
2020-01-14 20:43:21 +0100

    banner

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
 $(document).ready(function () {
         $('.sidenav').sidenav();
 });
-$(document).ready(function () {
-        $('.collapsible').collapsible();
-});
-$(document).ready(function () {
-        $('.datepicker').datepicker();
-});
-$(document).ready(function () {
-        $('.timepicker').timepicker({
-                "twelveHour": false
-        });
-});
-$(document).ready(function () {
-	$('modal').modal()
-        $('#modalSelectSong').modal({
-		'onOpenStart': update_sounds_list
-	});
-});
-
-$(document).ready(function () {
-        M.updateTextFields();
-});
